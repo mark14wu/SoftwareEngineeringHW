d01b2d5eed544185e9ca0a037fd3969edb2399ec
Mark Wu <mark14@mail.ustc.edu.cn>
2019-04-01 01:24:56 +0800

    bug fixed

--- hw1/src/Core.cpp
+++ hw1/src/Core.cpp
@@ -1,12 +1,13 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include "WordNode.cpp"
 using namespace std;
 
 class Core {
 public:
-	static int gen_chain(vector<string> &words, int len, vector<string> &result) {
+	static int gen_chain(vector<string> words, int len, vector<string> &result) {
 		result.clear();
 		WordNode WordMatrix[26][26];
 		for (int i = 0; i < 26; i++) {
@@ -47,7 +48,7 @@ public:
 		return n;
 	}
 
-	static int gen_chain_char(vector<string> &words, int len, vector<string> &result) {
+	static int gen_chain_char(vector<string> words, int len, vector<string> &result) {
 		result.clear();
 		WordNode WordMatrix[26][26];
 		for (int i = 0; i < 26; i++) {
@@ -88,7 +89,7 @@ public:
 		return n;
 	}
 
-	static int gen_chain_word(vector<string> &words, int len, vector<string> &result, char head, char tail) {
+	static int gen_chain_word(vector<string> words, int len, vector<string> &result, char head, char tail) {
 		result.clear();
 		WordNode WordMatrix[26][26];
 		for (int i = 0; i < 26; i++) {
@@ -131,7 +132,7 @@ public:
 		return n;
 	}
 
-	static void gen_chain_n_word(vector<string> &words, int len, int num, ofstream &outfile) {
+	static void gen_chain_n_word(vector<string> words, int len, int num, ofstream &outfile) {
 		WordNode WordMatrix[26][26];
 		for (int i = 0; i < 26; i++) {
 			for (int j = 0; j < 26; j++) {
@@ -294,7 +295,7 @@ private:
 				if (length == n_length) {
 					n++;
 					for (vector<string>::iterator it = temp_result.begin(); it != temp_result.end(); it++) {
-						outfile << *it << " ";
+						outfile << (*it) << " ";
 						cout << *it << " ";
 					}
 					outfile << endl;
--- hw1/src/WordNode.cpp
+++ hw1/src/WordNode.cpp
@@ -1,8 +1,10 @@
+#include <vector>
+
 class WordNode{
 public:
     char prefix;
     char suffix;
-    vector <string> wordlist;
+    std::vector <std::string> wordlist;
     int word_count;
     int word_use;
 };
